a632a0eb6cdf307eba325fcc16a699fe79c9a81b
Nicholas Petty <nickpetty@hotmail.com>
2017-10-02 22:05:01 -0400

    Repo creatd, first day's practice added.

---
+++ Module1Demo/Module1Demo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module1Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create variables of different data types
+            // Initialize with a "default" value
+            string firstName, lastName, street, city, country = "";
+            int age = 0;
+            DateTime birthDate;
+
+            // Assign some values
+            firstName = "John";
+            lastName = "Smith";
+            age = 17;
+            street = "1010 Sub Sandwich Street";
+            city = "Fries";
+            country = "Lunch";
+            birthDate = new DateTime(2017, 1, 1);
+
+            // Output to the console window
+
+            // Use simple output with just variable name
+            Console.WriteLine(firstName);
+            Console.WriteLine(lastName);
+
+            // Use placeholder style
+            Console.WriteLine("{0} years old", age);
+
+            // Use string concatenation
+            Console.WriteLine(street + ", " + city + ", " + country);
+
+            //Use string interpolation
+            Console.WriteLine($"Born on {birthDate}");
+        }
+    }
+}
