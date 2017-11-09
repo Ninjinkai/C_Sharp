04ed65d06c41304b6153d80c085ab0c8fd8334c2
Nicholas Petty <nickpetty@hotmail.com>
2017-11-08 22:53:33 -0500

    Some practice projects.

---
+++ TestApp/TestAppUnitTests/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestAppUnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            TestApp.MathOperations.Double(10);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(20, TestApp.MathOperations.Double(10));
+            Assert.AreEqual(30, TestApp.MathOperations.Triple(10));
+            Assert.AreEqual(0, TestApp.MathOperations.Double(0));
+        }
+    }
+}
