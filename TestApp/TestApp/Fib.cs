9f51b87a04e03cfad34bf00e7a14465f25d3b698
Nicholas Petty <nickpetty@hotmail.com>
2017-11-27 22:17:11 -0500

    Added Fibonacci program and unit tests.

---
+++ TestApp/TestApp/Fib.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    public class Fib
+    {
+        public static int RecursiveFib(int n)
+        {
+            if (n <0)
+            {
+                return -1;
+            }
+
+            if (n <= 1)
+            {
+                return n;
+            }
+            else
+            {
+                return RecursiveFib(n - 1) + RecursiveFib(n - 2);
+            }
+        }
+    }
+}
---
+++ TestApp/TestAppUnitTests/UnitTest2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestAppUnitTests
+{
+    [TestClass]
+    public class FibTests
+    {
+        [TestMethod]
+        public void TestRecursiveFibNegatives()
+        {
+            Assert.AreEqual(-1, TestApp.Fib.RecursiveFib(-1));
+            Assert.AreEqual(-1, TestApp.Fib.RecursiveFib(-100));
+        }
+
+        [TestMethod]
+        public void TestRecursiveFibBases()
+        {
+            Assert.AreEqual(0, TestApp.Fib.RecursiveFib(0));
+            Assert.AreEqual(1, TestApp.Fib.RecursiveFib(1));
+            Assert.AreEqual(1, TestApp.Fib.RecursiveFib(2));
+        }
+
+        [TestMethod]
+        public void TestRecursiveFib()
+        {
+            Assert.AreEqual(3, TestApp.Fib.RecursiveFib(4));
+            Assert.AreEqual(5, TestApp.Fib.RecursiveFib(5));
+            Assert.AreEqual(8, TestApp.Fib.RecursiveFib(6));
+        }
+    }
+}
