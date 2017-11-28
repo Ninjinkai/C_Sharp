f19935953c66f14e57b42547665c925a443a9512
Nicholas Petty <nickpetty@hotmail.com>
2017-11-27 22:20:42 -0500

    Refactored recursive Fibonacci method for clarity.

--- TestApp/TestApp/Fib.cs
+++ TestApp/TestApp/Fib.cs
@@ -10,12 +10,11 @@ namespace TestApp
     {
         public static int RecursiveFib(int n)
         {
-            if (n <0)
+            if (n < 0)
             {
                 return -1;
             }
-
-            if (n <= 1)
+            else if (n == 0 || n == 1)
             {
                 return n;
             }
